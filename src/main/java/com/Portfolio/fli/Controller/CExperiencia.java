4f0ffbc2fb9d241959ad9e41ffb26befe09f5899
Fair Lucas <lukitaa.27@gmail.com>
2023-04-13 17:35:00 -0300

    hys

--- src/main/java/com/Portfolio/fli/Controller/CExperiencia.java
+++ src/main/java/com/Portfolio/fli/Controller/CExperiencia.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = {"https://forntendprueba.web.app"})
+@CrossOrigin(origins = {"https://forntendprueba.web.app","http://localhost:4200"})
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
--- src/main/java/com/Portfolio/fli/Controller/PersonaController.java
+++ src/main/java/com/Portfolio/fli/Controller/PersonaController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = {"https://forntendprueba.web.app"})
+@CrossOrigin(origins = {"https://forntendprueba.web.app","http://localhost:4200"})
 public class PersonaController {
     @Autowired ImpPersonaService impPersonaService;
     /*@Autowired IPersonaService ipersonaService;
