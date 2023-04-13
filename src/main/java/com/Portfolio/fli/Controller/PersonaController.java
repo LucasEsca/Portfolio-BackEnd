13f3c4e1ee709f6d30a63beffbb47565d7916095
Fair Lucas <lukitaa.27@gmail.com>
2023-04-13 14:14:07 -0300

    crear persona old

--- src/main/java/com/Portfolio/fli/Controller/PersonaController.java
+++ src/main/java/com/Portfolio/fli/Controller/PersonaController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +29,11 @@ public class PersonaController {
         return ipersonaService.getPersona();
     }
     
+    @PostMapping("/personas/crear")
+    public String createPersona(@RequestBody Persona persona){
+        ipersonaService.savePersona(persona);
+        return "La persona fue creada correctamente";
+    }
  @GetMapping("/lista")
     public ResponseEntity<List<Persona>> list(){
         List<Persona> list = impPersonaService.list();
